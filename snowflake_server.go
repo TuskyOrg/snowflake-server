6cec4f951f736bd9502de29f8b92a53973936397
Snapper <LearningWithSnapper@gmail.com>
2021-06-29 16:50:06 -0400

    Changed "id"'s type from 'uint64' -> 'string' so server could handle large numbers;

---
+++ snowflake_server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/AmreeshTyagi/goldflake"
+	"net/http"
+	"strconv"
+)
+
+var gf *goldflake.Goldflake
+
+func getMachineID() (uint16, error) {
+	return 1234, nil
+}
+
+func init() {
+	var settings goldflake.Settings
+	settings.MachineID = getMachineID
+	gf = goldflake.NewGoldflake(settings)
+	if gf == nil {
+		panic("goldflake not created")
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	id, err := gf.NextID()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("%v\n", id)
+	body, err := json.Marshal(
+		map[string]string{
+			"id": strconv.FormatUint(id, 10),
+		})
+	fmt.Printf("%d\n", id)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header()["Content-Type"] = []string{"application/json; charset=utf-8"}
+	_, _ = w.Write(body)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
